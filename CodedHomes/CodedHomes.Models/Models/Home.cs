4c89d90b465df3b2e27695544dc420862b1c8b53
trevor <trevor_jed_smith@hotmail.com>
2017-06-23 00:09:32 +0930

    Cleanup of code - just functional testing done

--- CodedHomes/CodedHomes.Models/Models/Home.cs
+++ CodedHomes/CodedHomes.Models/Models/Home.cs
@@ -1,6 +1,7 @@
 ﻿using CodedHomes.Models.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,22 @@ namespace CodedHomes.Models
     public class Home : IAuditInfo
     {
         public int Id { get; set; }
+        [Required]
         public string StreetAddress { get; set; }
         public string StreetAddress2 { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public int ZipCode { get; set; }
+        [Required]
         public int? Bathrooms { get; set; }
+        [Required]
         public int? Bedrooms { get; set; }
+        [Required]
         public int? SquareFeet { get; set; }
+        [Required]
         public int? Price { get; set; }
+        [Required]
         public string Description { get; set; }
         public string ImageName { get; set; }
         public DateTime CreatedOn { get; set; }
